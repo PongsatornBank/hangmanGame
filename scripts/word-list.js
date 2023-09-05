5b8b8874c9bbcb4f64ccc7e31697946612f8de26
PongsatornBank <136670624+PongsatornBank@users.noreply.github.com>
2023-09-05 13:02:00 +0700

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,42 @@
+const wordList = [
+    {
+        word: "binary",
+        hint: "A numbers expressed in the base of two, using 0 and 1 only"
+    },
+    {
+        word: "ternary",
+        hint: "A numbers expressed in the base of three, instead of 10, using 0, 1 and 2 only"
+    },
+    {
+        word: "octal",
+        hint: "A numbers expressed in the base of eight, instead of 10, using 0, 1, 2,3 4, 5, 6 and 7 only"
+    },
+    {
+        word: "decimal",
+        hint: "A numbers expressed in the base of 10, using 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9 only"
+    },
+    {
+        word: "hexadecimal",
+        hint: "A numbers expressed in the base of sixteen, instead of 10, using 0, 1, 2,3 4, 5, 6, 7, 8, 9, a, b, c, d, e ond f"
+    },
+    {
+        word: "char",
+        hint: "A single display unit of information equivalent to one alphabetic symbol, digit, or letter"
+    },
+    {
+        word: "string",
+        hint: "A data type used in programming languages, representing text"
+    },
+    {
+        word: "integer",
+        hint: "A data type in programming languages, representing a whole number positive or negative"
+    },
+    {
+        word: "floating",
+        hint: "A data type in programming languages, representing a type of number, with a decimal point"
+    },
+    {
+        word: "boolean",
+        hint: "A simple data type having just two possible values: true and false"
+    },  
+];
